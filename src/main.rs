caed47cd07531fd1812f4136580b73d268931dca
Jayanth Manklu <jayanthms7@gmail.com>
2019-10-20 22:10:25 +0530

    Part 1 complete!
    - build using a ARM executable target thumbv7em-none-eabihf
    - rustup target add thumbv7em-none-eabihf
    - cargo build --target thumbv7em-none-eabihf
    - this provides bare bones runtime which is not C hence you dont get C linker errors when building anymore

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-#![no_std]
-#![no_main]
+#![no_std]  // do not link the rust standard library and transitively C runtime from host OS
+#![no_main] // disable rust level entry point called by C runtime crt0
 
 use core::panic::PanicInfo;
 
+// this function is called on panic, defining our own because std library of rust provides this
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // do not mangle the name of this function, as it will be used by our linker as entry point
 pub extern "C" fn _start() -> ! {
     loop {}
 }
