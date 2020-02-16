6d10d508f7c9a428eb70aa02edda739a02d9c798
Jayanth Manklu <jayanthms7@gmail.com>
2020-02-16 14:34:06 +0530

    Part 2 Complete!
    - create a custom target for cargo to compile to, with config
      on linker setup, llvm target, pointer widths and all
    - use xbuild crate to build rust "core" + our code to this target
    - use bootimage crate to compile "kernel" - our code - to link to a standard bootloader
    - write "hello world" to standard VGA buffer in the entrypoint
      of the kernel using raw pointers
    - boot system into a QEMU VM by using executable from bootimage step

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"HELLO WORLD!";
+
 #[no_mangle] // do not mangle the name of this function, as it will be used by our linker as entry point
+// this function is the entry_point, linker looks specifically for a function named `_start`
 pub extern "C" fn _start() -> ! {
+    let vga_buffer_addr = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer_addr.offset(i as isize * 2) = byte;
+            *vga_buffer_addr.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
