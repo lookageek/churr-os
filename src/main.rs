0e9e12e85e5044b9ac51c848e5455676c6e2df10
Jayanth Manklu <jayanthms7@gmail.com>
2020-02-16 20:04:37 +0530

    Part 3 - VGA Buffer
    - a new module having types and methods to provide safe API to VGA Buffer handling current cursor in buffer,
      ASCII characters, colors and all the paraphernalia
    - implementing fmt::write trait to provide convenient write! macro support for the buffer
    - expose a lazy static buffer writer wrapped in a spinlock mutex as the principal API for the buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]  // do not link the rust standard library and transitively C runtime from host OS
 #![no_main] // disable rust level entry point called by C runtime crt0
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // this function is called on panic, defining our own because std library of rust provides this
@@ -14,13 +16,8 @@ static HELLO: &[u8] = b"HELLO WORLD!";
 #[no_mangle] // do not mangle the name of this function, as it will be used by our linker as entry point
 // this function is the entry_point, linker looks specifically for a function named `_start`
 pub extern "C" fn _start() -> ! {
-    let vga_buffer_addr = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer_addr.offset(i as isize * 2) = byte;
-            *vga_buffer_addr.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
     loop {}
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,150 @@
+use volatile::Volatile;
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+// all the colors which VGA text buffer supports in its 4 bit space
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+// colorcode - 8 bit, 4 bits of text color, 4 bits of background color
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+// a single character in the text buffer,
+// ascii byte and what color should the char should look
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+// buffer of 80 characters, 25 lines, starting at address location 0xb8000
+// which is memory mapped location of VGA device text buffer
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+// state of the buffer - what row and column we are currently writing to,
+// what color to use
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+// initialise a lazy static writer to be used as module API to the buffer
+// under a spinlock mutex to make it "mutable static"
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+impl Writer {
+    // writes a byte to text buffer and appropriately updates the cursors
+    // if a new line byte is passed, new_line method is called, or else byte is written
+    // to buffer
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    // move all the characters already in buffer one line above and empty out
+    // the bottom most line, the top most line is shifted off the screen like a
+    // scroll view
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    // clears out the row by writing space in all positions of row
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+    // take a string, iterate over the bytes, if it is a valid byte then
+    // write to buffer, or write a square character
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    // implementing write! macro, just delegate to `write_string` method already written
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
