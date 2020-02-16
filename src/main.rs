88a0193d02d3168efd10f302082c6c6061c3a736
Jayanth Manklu <jayanthms7@gmail.com>
2020-02-16 20:11:27 +0530

    Part 3 - VGA Buffer
    - implement print and println macro for the buffer writer
    - use println macro in main.rs and also in panic handler

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use core::panic::PanicInfo;
 
 // this function is called on panic, defining our own because std library of rust provides this
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -16,8 +17,7 @@ static HELLO: &[u8] = b"HELLO WORLD!";
 #[no_mangle] // do not mangle the name of this function, as it will be used by our linker as entry point
 // this function is the entry_point, linker looks specifically for a function named `_start`
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
